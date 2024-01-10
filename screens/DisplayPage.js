e230e61303ed22c3fa2d4c7d4eeb0a801e25e41c
AhmAlkalii <hmkalkali@gmail.com>
2024-01-10 23:52:19 +0100

    test

--- screens/DisplayPage.js
+++ screens/DisplayPage.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { View, SafeAreaView, Text, TextInput,StatusBar, Pressable,StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import DateTimePicker from "@react-native-community/datetimepicker";
-=======
-import React from "react";
-import { View, SafeAreaView, Text, Button, StyleSheet } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
->>>>>>> 38e93b095d8e23b48f307b3ea241c17bb674d9fe
 
 export default function DisplayPage({ navigation, route }) {
   const [value, setSearchValue] = useState("");
@@ -29,7 +23,6 @@ export default function DisplayPage({ navigation, route }) {
 
   return (
     <SafeAreaView style={styles.safecontainer}>
-<<<<<<< HEAD
         <StatusBar barStyle={'dark-content'}/>
 
         <View style={styles.header}>
@@ -109,27 +102,6 @@ export default function DisplayPage({ navigation, route }) {
           </Pressable>
         </View>
 
-=======
-      <View style={styles.header}>
-        <Text style={styles.text}>Rentals</Text>
-      </View>
-      <View style={styles.border}>
-        <Icon name="search" size={22} color="black" />
-        <Text style={styles.searchtext}>Pick-up and Return locations</Text>
-      </View>
-
-      <View style={styles.borderContainer}>
-        <View style={styles.secborder}></View>
-
-        <View style={styles.secborder}></View>
-      </View>
-
-      <View style={styles.borderContainer}>
-        <View style={styles.secborder}></View>
-
-        <View style={styles.secborder}></View>
-      </View>
->>>>>>> 38e93b095d8e23b48f307b3ea241c17bb674d9fe
     </SafeAreaView>
   );
   
@@ -142,27 +114,19 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "bold",
-    paddingHorizontal: 24,
+    paddingHorizontal:24,
     marginBottom: 16,
   },
-<<<<<<< HEAD
   searchinput:{
     alignSelf:'center',
     fontSize:18,
     paddingHorizontal:10
-=======
-  searchtext: {
-    alignSelf: "center",
-    fontSize: 18,
-    paddingHorizontal: 13,
->>>>>>> 38e93b095d8e23b48f307b3ea241c17bb674d9fe
   },
-  border: {
-    backgroundColor: "#f5f5f5",
+  border:{
+    backgroundColor: '#f5f5f5',
     borderRadius: 17,
     borderWidth: 1,
     padding: 16,
-<<<<<<< HEAD
     margin: 14,
     marginHorizontal:23,
     flexBasis:60,
@@ -172,22 +136,12 @@ const styles = StyleSheet.create({
   borderContainer:{
     flexDirection:'row',
     paddingHorizontal:24,
-=======
-    paddingHorizontal: 17,
-    margin: 10,
-    flexBasis: 60,
-    flexDirection: "row",
   },
-  borderContainer: {
-    flexDirection: "row",
->>>>>>> 38e93b095d8e23b48f307b3ea241c17bb674d9fe
-  },
-  secborder: {
-    backgroundColor: "#f5f5f5",
+  secborder:{
+    backgroundColor: '#f5f5f5',
     borderRadius: 17,
     borderWidth: 1,
     padding: 16,
-<<<<<<< HEAD
     margin: 10,
     height:70,
     width: 140,
@@ -217,12 +171,3 @@ const styles = StyleSheet.create({
     margin:9
   }
 });
-=======
-    paddingHorizontal: 17,
-    margin: 10,
-    height: 70,
-    width: 168,
-    flexDirection: "row",
-  },
-});
->>>>>>> 38e93b095d8e23b48f307b3ea241c17bb674d9fe
