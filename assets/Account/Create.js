637eba920b89819621cd0d6f46f564fdf5893270
abdull56 <abdullshubby@gmail.com>
2024-01-11 18:52:08 +0100

    feat: create sign up page

---
+++ assets/Account/Create.js
@@ -0,0 +1,121 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
+// import CheckBox from "@react-native-community/checkbox";
+import { useState } from "react";
+
+const Create = () => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+
+  return (
+    <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.text}>Create Account</Text>
+        <Text style={styles.text2}>Create Account to access existing jobs</Text>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>First Name </Text>
+          <TextInput style={styles.input} placeholder="first name" />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Last Name </Text>
+          <TextInput style={styles.input} placeholder="last name" />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Email </Text>
+          <TextInput style={styles.input} placeholder="email" />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            style={styles.button}
+            title="Sign Up"
+            color="white"
+            onPress={() => console.log("Sign Up")}
+          />
+        </View>
+
+        <View style={styles.privacyContainer}>
+          <Text>I agree to privacy policy</Text>
+        </View>
+
+        <View style={styles.privacyContainer}>
+          {/* <CheckBox
+            disabled={false}
+            value={toggleCheckBox}
+            onValueChange={(newValue) => setToggleCheckBox(newValue)}
+          /> */}
+          <Text>Already have an account ? Sign in </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#fff",
+    paddingTop: 20,
+    marginTop: 60,
+  },
+
+  text: {
+    color: "purple",
+    fontWeight: "bold",
+    fontSize: 35,
+    paddingBottom: 10,
+  },
+
+  text2: {
+    marginBottom: 30,
+  },
+
+  inputContainer: {
+    marginTop: 10,
+    borderColor: "purple",
+  },
+
+  inputText: {
+    paddingTop: 10,
+    marginBottom: 10,
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+
+  input: {
+    paddingTop: 5,
+    borderColor: "purple",
+    borderWidth: 1,
+    padding: 40,
+    width: 370,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    marginTop: 30,
+  },
+
+  buttonContainer: {
+    backgroundColor: "skyblue",
+    marginTop: 30,
+    padding: 15,
+    borderRadius: 10,
+  },
+
+  privacyContainer: {
+    marginTop: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+  },
+});
+export default Create;
