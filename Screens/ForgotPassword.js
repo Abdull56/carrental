c4557641bc7cf63ee753ce9bd7b28020fc9da5f0
abdull56 <abdullshubby@gmail.com>
2024-01-20 22:25:03 +0100

    chore:minor changes

--- Screens/ForgotPassword.js
+++ Screens/ForgotPassword.js
@@ -1,33 +1,64 @@
 import React from "react";
-import { Text, View, ScrollView, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
 import { globalStyles } from "../Styles/Styles";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required").email("Invalid email"),
+  password: yup.string().required("Password is required"),
+});
 
 export default function ForgotPassword() {
   return (
-    <ScrollView>
-      <View style={globalStyles.container}>
-        <View>
-          <Text style={globalStyles.text}>ForgotPassword</Text>
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={schema}
+        onSubmit={(value, actions) => {
+          console.log(value);
+        }}
+      >
+        {(props) => (
+          <View>
+            <View>
+              <Text style={globalStyles.text}>ForgotPassword</Text>
 
-          <View style={globalStyles.inputContainer}>
-            <Text style={globalStyles.inputText}>Email</Text>
-            <TextInput style={globalStyles.input} placeholder="full name" />
-          </View>
+              <View style={globalStyles.inputContainer}>
+                <Text style={globalStyles.inputText}>Email</Text>
+                <TextInput
+                  style={globalStyles.input}
+                  placeholder="email"
+                  onChangeText={props.handleChange("email")}
+                  value={props.values.email}
+                />
+                <Text style={{ color: "red" }}>{props.errors.email}</Text>
+              </View>
 
-          <View style={globalStyles.inputContainer}>
-            <Text style={globalStyles.inputText}>Password </Text>
-            <TextInput
-              style={globalStyles.input}
-              placeholder="password"
-              secureTextEntry
-            />
-          </View>
+              <View style={globalStyles.inputContainer}>
+                <Text style={globalStyles.inputText}>Password </Text>
+                <TextInput
+                  style={globalStyles.input}
+                  placeholder="password"
+                  secureTextEntry
+                  onChangeText={props.handleChange("password")}
+                  value={props.values.password}
+                />
+                <Text style={{ color: "red" }}>{props.errors.password}</Text>
+              </View>
 
-          <View style={globalStyles.buttonContainer}>
-            <Button style={globalStyles.button} title="Verify" color="white" />
+              <View style={globalStyles.buttonContainer}>
+                <Button
+                  style={globalStyles.button}
+                  title="Verify"
+                  color="white"
+                  // onPress={props.handleSubmit("")}
+                />
+              </View>
+            </View>
           </View>
-        </View>
-      </View>
-    </ScrollView>
+        )}
+      </Formik>
+    </View>
   );
 }
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,38 +1,62 @@
 import React from "react";
-import { Text, View, ScrollView, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
 import { globalStyles } from "../Styles/Styles";
+import * as yup from "yup";
+import { Formik } from "formik";
+
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required").email("Invalid email"),
+  password: yup.string().required("Password is required"),
+});
 
 export default function Login({ navigation }) {
   return (
-    <ScrollView>
-      <View style={globalStyles.container}>
-        <View>
-          <Text style={globalStyles.text}>Login</Text>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={schema}
+      onSubmit={(value, actions) => {
+        console.log(value);
+      }}
+    >
+      {(props) => (
+        <View style={globalStyles.container}>
+          <View>
+            <Text style={globalStyles.text}>Login</Text>
 
-          <View style={globalStyles.inputContainer}>
-            <Text style={globalStyles.inputText}>Full Name </Text>
-            <TextInput style={globalStyles.input} placeholder="full name" />
-          </View>
+            <View style={globalStyles.inputContainer}>
+              <Text style={globalStyles.inputText}>Email </Text>
+              <TextInput
+                style={globalStyles.input}
+                placeholder="email"
+                onChangeText={props.handleChange("email")}
+                value={props.values.email}
+              />
+              <Text style={{ color: "red" }}>{props.errors.email}</Text>
+            </View>
 
-          <View style={globalStyles.inputContainer}>
-            <Text style={globalStyles.inputText}>Password </Text>
-            <TextInput
-              style={globalStyles.input}
-              placeholder="password"
-              secureTextEntry
-            />
-          </View>
+            <View style={globalStyles.inputContainer}>
+              <Text style={globalStyles.inputText}>Password </Text>
+              <TextInput
+                style={globalStyles.input}
+                placeholder="password"
+                secureTextEntry
+                onChangeText={props.handleChange("password")}
+                value={props.values.password}
+              />
+              <Text style={{ color: "red" }}>{props.errors.password}</Text>
+            </View>
 
-          <View style={globalStyles.buttonContainer}>
-            <Button
-              style={globalStyles.button}
-              title="Login"
-              color="white"
-              onPress={() => navigation.navigate("ForgotPassword")}
-            />
+            <View style={globalStyles.buttonContainer}>
+              <Button
+                style={globalStyles.button}
+                title="Login"
+                color="white"
+                onPress={() => navigation.navigate("ForgotPassword")}
+              />
+            </View>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      )}
+    </Formik>
   );
 }
