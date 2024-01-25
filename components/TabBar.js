b1bd83a5cbc1f92e7af9ab12bc88dac600d1ee2d
abdull56 <abdullshubby@gmail.com>
2024-01-25 23:32:13 +0100

    chore: minor changes

--- components/TabBar.js
+++ components/TabBar.js
@@ -3,7 +3,6 @@ import "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import DisplayPage from "../Screens/DisplayPage";
 import StackBar from "./StackBar";
-import { globalStyles } from "./styles/globalStyles";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Settings from "../Screens/Settings";
 
@@ -12,36 +11,59 @@ const Tab = createBottomTabNavigator();
 function TabBar() {
   return (
     <Tab.Navigator
-      barStyle={globalStyles.tabcontainer}
-      screenOptions={{ headerShown: false }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#32de84",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
       <Tab.Screen
         name="Home"
         component={StackBar}
         options={{
-          tabBarIcon: () => <Ionicons name="home-outline" size={20} />,
+          tabBarIcon: () => (
+            <Ionicons name="home-outline" size={20} color="#006A4E" />
+          ),
         }}
       />
       <Tab.Screen
         name="Profile"
         component={Settings}
         options={{
-          tabBarIcon: () => <Ionicons name="person" size={20} />,
-          tabBarBadge: 3,
+          tabBarIcon: () => (
+            <Ionicons name="person" size={20} color="#006A4E" />
+          ),
+          // tabBarBadge: 3,
         }}
       />
       <Tab.Screen
         name="Settings"
         component={Settings}
         options={{
-          tabBarIcon: () => <Ionicons name="settings-outline" size={20} />,
+          tabBarIcon: () => (
+            <Ionicons name="settings-outline" size={20} color="#006A4E" />
+          ),
         }}
       />
       <Tab.Screen
         name="Messages"
         component={DisplayPage}
         options={{
-          tabBarIcon: () => <Ionicons name="chatbubble-outline" size={20} />,
+          tabBarIcon: () => (
+            <Ionicons name="chatbubble-outline" size={20} color="#006A4E" />
+          ),
         }}
       />
     </Tab.Navigator>
--- components/styles/globalStyles.js
+++ components/styles/globalStyles.js
@@ -1,103 +1,102 @@
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    safecontainer: {
-      flex: 1,
-      backgroundColor:'white'
-    },
-    text: {
-      fontSize: 24,
-      fontWeight: "bold",
-      paddingHorizontal:18,
-      marginBottom: 16,
-    },
-    searchinput:{
-      alignSelf:'center',
-      fontSize:18,
-      paddingHorizontal:10
-    },
-    border:{
-      backgroundColor: '#f5f5f5',
-      borderRadius: 17,
-      borderWidth: 1,
-      padding: 16,
-      margin: 14,
-      marginHorizontal:23,
-      flexBasis:60,
-      width:330,
-      flexDirection: 'row',
-    },
-    borderContainer:{
-      flexDirection:'row',
-      paddingHorizontal:24,
-    },
-    secborder:{
-      backgroundColor: 'white',
-      borderRadius: 17,
-      borderWidth: 1,
-      padding: 16,
-      margin: 10,
-      height:70,
-      width: 140,
-      justifyContent:'center'
-    },
-    icondate:
-    {
-      flexDirection:'row',
-    },
-    datePicker:{
-      width:100,
-      marginTop:4,
-      backgroundColor: 'white',
-    },
-    datePickerLabel:{
-      marginHorizontal:4,
-      color:'#AAD7D9'
-    },
-    button:{
-      backgroundColor: '#90EE90',
-      borderRadius: 26,
-      borderWidth: 1,
-      flexBasis:60,
-      width:337
-    },
-    buttonContainer:{
-      alignItems:'center',
-      margin:9
-    },
-    carContainer:{
-      margin:6,
-    },
-    cardIntro:{
-      flexDirection:'row',
-      justifyContent:'space-between'
-    },
-    card:{
-      backgroundColor: 'white',
-      padding:16,
-      alignSelf:'center',
-      borderRadius:8,
-      borderWidth:1,
-      width:350
-    },
-    cardText:{
-      fontSize:15,
-      fontWeight:'bold'
-    },
-    imageCard:{
-        flexDirection:'row',
-    },
-    cardCars:{
-        flexDirection:'row',
-        justifyContent:'space-between'
-    },
-    image:{
-      width:'100%',
-      height: 170,
-      marginBottom:16,
-      marginLeft: -10 
-    },
-    tabcontainer:{
-        backgroundColor:'white'
-    }
-  });
+  safecontainer: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingHorizontal: 18,
+    marginBottom: 16,
+  },
+  searchinput: {
+    alignSelf: "center",
+    fontSize: 18,
+    paddingHorizontal: 10,
+  },
+  border: {
+    backgroundColor: "#f5f5f5",
+    borderRadius: 17,
+    borderWidth: 1,
+    padding: 16,
+    margin: 14,
+    marginHorizontal: 23,
+    flexBasis: 60,
+    width: 330,
+    flexDirection: "row",
+  },
+  borderContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 24,
+  },
+  secborder: {
+    backgroundColor: "white",
+    borderRadius: 17,
+    borderWidth: 1,
+    padding: 16,
+    margin: 10,
+    height: 70,
+    width: 140,
+    justifyContent: "center",
+  },
+  icondate: {
+    flexDirection: "row",
+  },
+  datePicker: {
+    width: 100,
+    marginTop: 4,
+    backgroundColor: "white",
+  },
+  datePickerLabel: {
+    marginHorizontal: 4,
+    color: "#AAD7D9",
+  },
+  button: {
+    backgroundColor: "#90EE90",
+    borderRadius: 26,
+    borderWidth: 1,
+    flexBasis: 60,
+    width: 337,
+  },
+  buttonContainer: {
+    alignItems: "center",
+    margin: 9,
+  },
+  carContainer: {
+    margin: 6,
+  },
+  cardIntro: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    alignSelf: "center",
+    borderRadius: 8,
+    borderWidth: 1,
+    width: 350,
+  },
+  cardText: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  imageCard: {
+    flexDirection: "row",
+  },
+  cardCars: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: "100%",
+    height: 170,
+    marginBottom: 16,
+    marginLeft: -10,
+  },
+  tabcontainer: {
+    backgroundColor: "red",
+  },
+});
