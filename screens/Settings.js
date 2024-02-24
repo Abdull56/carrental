d9dc3c3ea3c86db92f3615b6c5e7aedb3bedb935
abdull56 <abdullshubby@gmail.com>
2024-02-24 18:10:01 +0100

    chore: minor changes.

--- screens/Settings.js
+++ screens/Settings.js
@@ -220,7 +220,7 @@ const styles = StyleSheet.create({
     width: 28,
     height: 28,
     borderRadius: 9999,
-    backgroundColor: "#007bff",
+    backgroundColor: "gray",
   },
   profileName: {
     marginTop: 20,
